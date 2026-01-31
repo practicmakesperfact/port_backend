968fe0e1b028e59410cec14db59bffee71ba3f9d
Haymanot Asmare <hasmare463@gmail.com>
2026-01-31 05:43:09 -0800

    Update CORS_ALLOWED_ORIGINS to correct Vercel app URL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://haymanotasmare.vercel.app', 
+    'https://haymanot.vercel.app', 
     # You can keep the config() version too:
     *config(
         'CORS_ALLOWED_ORIGINS',
